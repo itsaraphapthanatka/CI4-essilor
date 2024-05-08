af114c79cef30625a3da226ca5c3c66edb8e3710
itsaraphaptop <itsaraphap.top@gmail.com>
2024-05-08 23:53:14 +0700

    dd

--- app/Config/App.php
+++ app/Config/App.php
@@ -17,7 +17,7 @@ class App extends BaseConfig
      *
      *    http://example.com/
      */
-    public string $baseURL = 'https://host-workflow/';
+    public string $baseURL = 'https://host-essilor/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
